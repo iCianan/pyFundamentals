a27fafbfdbbd6d6410b307b7bb911856bd45ed12
Keenan Irvin-Benning <keenan@catalystdigitalventures.com>
2019-11-09 18:03:25 -0500

    BFS graph traversal

--- Data Structures/Graph.py
+++ Data Structures/Graph.py
@@ -1,3 +1,5 @@
+from collections import deque
+
 class Graph:
     def __init__(self):
         self.adjacencyList = {}
@@ -8,21 +10,39 @@ class Graph:
             self.adjacencyList[node] = []
             self.length += 1
 
-    def addEdges(self, node1, node2):
-        if node1 in self.adjacencyList and node2 in self.adjacencyList:
-            self.adjacencyList[node1].append(node2)
-            self.adjacencyList[node2].append(node1)
+    def addEdges(self, u, v):
+        if u in self.adjacencyList and v in self.adjacencyList:
+            self.adjacencyList[u].append(v)
+            self.adjacencyList[v].append(u)
+
+    def BFS(self, vertex):
+        queue = deque()
+        visited = []
+        queue.append(vertex)
+        while len(queue):
+            current = queue.popleft()
+            if current not in visited:
+                visited.append(current)
+                neighbors = self.adjacencyList[current]
+                for neighbor in neighbors:
+                    queue.append(neighbor)
+        return visited
+
+
 
 
 def main():
     graph = Graph()
     graph.addVertex('Bitcoin')
-    graph.addVertex('Litcoin')
+    graph.addVertex('Litecoin')
     graph.addVertex('ZCash')
     graph.addVertex('Horizen')
     graph.addVertex('Ethereum')
     graph.addEdges('Bitcoin', 'Litecoin')
-    print(graph.length)
+    graph.addEdges('Litecoin', 'ZCash')
+    graph.addEdges('ZCash', 'Horizen')
+    graph.addEdges('Ethereum', 'Horizen')
+    print(graph.BFS('ZCash'))
 
 
 if __name__ == "__main__":
