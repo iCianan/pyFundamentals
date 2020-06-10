fec9c340ee135ceafe61def412d7d51f1293f4bb
Keenan Irvin Benning <keenan@catalystdigitalventures.com>
2020-06-10 08:14:27 -0400

    added cipher encryptor

---
+++ Questions/CaesarCipherEncryptor.py
@@ -0,0 +1,21 @@
+def caesarCipherEncryptor(string, key):
+  newLetters = []
+  newKey = key % 26
+  for letter in string:
+      newLetters.append(getNewLetter(letter, newKey))
+  return "".join(newLetters)
+
+
+def getNewLetter(letter, key):
+  newLetterCode = ord(letter) + key
+  return chr(newLetterCode) if newLetterCode <= 122 else chr(96 + newLetterCode % 122)
+
+
+def main():
+  test = 'abc'
+  key = 2
+  print(caesarCipherEncryptor(test, key))
+
+
+if __name__ == "__main__":
+  main()
